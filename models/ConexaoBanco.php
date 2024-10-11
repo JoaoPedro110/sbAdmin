70de50be9d71e23adeec9abdd02e54dbfc5ae5fb
João Pedro Magrini <reccojp65@gmail.com>
2024-10-11 08:55:00 -0300

    Initial commit

---
+++ models/ConexaoBanco.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once './config/init.php';
+
+class ConexaoBanco {
+    protected $conBD;
+
+    function __construct() {
+
+        if(isset($this->conBD) == false){
+            $this->fazerConexaoBanco();
+        }
+    }
+
+    function fazerConexaoBanco()
+    {
+        try {
+            $this->conBD = new PDO("mysql:host=" . DB_HOST . ";port=" . DB_PORT . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+
+        } catch (PDOException $e) {
+            echo "Erro ao \Conectar com o Banco de Dedos: " . $e->getMessage();
+        }
+    }
+}
